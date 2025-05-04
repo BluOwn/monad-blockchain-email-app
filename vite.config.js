9f0e74da63a931e38d038865a6986f73b86b0ed8
BluOwn <teba.741@gmail.com>
2025-05-04 11:06:19 +0400

    up

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
-  }
+  plugins: [react()]
 })
